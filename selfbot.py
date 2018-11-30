78238eb2af4883bacac1292ef5b279417f34391b
tawfiqsiam <toto.siam999@gmail.com>
2018-11-30 10:06:51 +0200

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2017 Grok
+Copyright (c) 2017 Gok
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
